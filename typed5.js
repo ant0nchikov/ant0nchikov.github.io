7bdbd9d51c4aa03ba86914461c9563606dac9652
ant0nchikov <ant0nchikov.aleksandr>
2019-05-10 13:10:32 +0400

    Delete typed5.js

--- typed5.js
+++
@@ -1,9 +0,0 @@
- var typed4 = new Typed('#typed5', {
-    strings: ['', 'Good afternoo! I want to use your services of a web developer!', 'I need a business card on the Internet for my business!', 'I need a website!'],
-    typeSpeed: 30,
-    backSpeed: 5,
-    attr: 'placeholder',
-    bindInputFocusEvents: true,
-    loop: true,
-    cursorChar: ''
-  });
